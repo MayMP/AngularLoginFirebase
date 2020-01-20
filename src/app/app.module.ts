620785b28a1983fede1b02ce9e7c636347a9bc7f
MayMP <nainghtetko87@gmail.com>
2020-01-21 01:12:18 +0800

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store'; 
+
+//Auth Service
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guard/auth-guard.service';
+
+// Firebase services + environment module
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+// Material design
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './styles/material.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+// Reducers
+import { UserReducer } from './store/reducers/user.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    StoreModule.forRoot({ yd: UserReducer })
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
+
+// Store data in project
+import { Store, select } from '@ngrx/store';
+import { UserState } from '../../store/reducers/user.reducer';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  userInfo: any;
+  userData: any = [];
+
+  constructor(
+    private store: Store<UserState>,
+    private dashboardService: DashboardService
+  ) {
+    this.store.pipe(select( (state: any) => state.yd.user )).subscribe(user => {
+      if( user['user'] ){
+        this.userInfo = user['user'];
+
+        console.log( this.userInfo, " >>>>>>>>> this.userInfo " );
+      } 
+    })
+  }
+
+  ngOnInit() {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    this.userData = undefined;
+    this.dashboardService.getUserData(this.userInfo.id).then(result => {
+      if( result != undefined )
+        this.userData = result;
+    });
+  }
+}
