5141945c41cbe0fb1f72cbf28cd993af60a9f08a
MayMP <nainghtetko87@gmail.com>
2020-01-21 01:26:45 +0800

    Second commit.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularPWA'`, () => {
+  it(`should have as title 'AngularLoginFirebase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngularPWA');
+    expect(app.title).toEqual('AngularLoginFirebase');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularPWA!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularLoginFirebase!');
   });
 });
